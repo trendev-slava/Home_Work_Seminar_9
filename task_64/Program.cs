e2b637c3856c583e748d8a362639da9c61cea033
Вячеслав Трендев <trendev97@gmail.com>
2023-04-30 16:26:03 +0500

    initial commit

---
+++ task_64/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = GetNumberInput("Введите число: ");
+ Console.WriteLine();
+PrintArrayNumbers(number);
+
+int GetNumberInput(string message = "")
+{
+    Console.WriteLine(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+void PrintArrayNumbers(int number)
+{
+    if(number == 1)
+    Console.WriteLine(number);
+    else 
+    {
+        Console.Write($"{number} ");
+        PrintArrayNumbers(number-1);
+    }
+    
+}
+
---
+++ task_66/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = GetNumberInput("N: ");
+int m = GetNumberInput("M: ");
+
+if (n < m) Console.WriteLine(SumOfElements(n, m));
+
+else Console.WriteLine(SumOfElements(m, n));
+
+int GetNumberInput(string message = "")
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int SumOfElements(int n, int m)
+{
+    if (n == m) return n;
+    else return SumOfElements(n + 1, m) + n;
+}
+
